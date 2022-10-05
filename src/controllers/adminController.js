9e73ce400ab7f38f491816aa29d0b42af5c0901c
Prajapati omkar <prajapatiomkar01@gmail.com>
2022-10-05 17:26:17 +0530

    Successfully Implemented Admin permission

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,5 @@
 const adminModel = require("../models/admin");
+const noteModel = require("../models/note");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
@@ -31,7 +32,8 @@ const adminLogin = async (req, res) => {
         // console.log(token)
         res.cookie("token", token);
         // res.redirect("/note/main")
-        res.send("Admin Login")
+        res.redirect("/admin/posts")
+
         // res.status(200).json({ user: existingUser, token: token })
     } catch (error) {
         console.log(error);
@@ -41,34 +43,74 @@ const adminLogin = async (req, res) => {
 const adminRegister = async (req, res) => {
     const { username, email, password, administratorpassword } = req.body;
 
-    if(administratorpassword === ADMINISTRATOR_PASSWORD){
+    if (administratorpassword === ADMINISTRATOR_PASSWORD) {
         try {
             const existingUser = await adminModel.findOne({ email: email });
             if (existingUser) {
                 return res.status(409).json({ message: "Administrator already exists" }) //400-bad request
             }
             const hashedPassword = await bcrypt.hash(password, 10);
-    
+
             const result = await adminModel.create({
                 email: email,
                 password: hashedPassword,
                 username: username,
             });
-    
+
             const token = jwt.sign({ email: result.email, id: result._id }, SECRECT_KEY);
             res.cookie("token", token);
             // console.log(token)
             // res.status(201).json({ user: result, token: token }) //201-successfully record created
-            res.send("Admin Register")
+            res.redirect("/admin/login")
         } catch (error) {
             console.log(error);
             res.status(500).json({ message: "Something went wrong" });
         }
-    }else{
+    } else {
         res.redirect("/admin/register")
     }
 
-   
+
 }
 
-module.exports = { adminLoginPage, adminRegisterPage, adminLogin, adminRegister };
+const getAllUserBlog = async (req, res) => {
+    try {
+        const notes = await noteModel.find({ isPublic: "true" });
+        res.render("adminPanel", { allBlogItemInMain: notes })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
+
+const getSingleBlog = (req, res) => {
+    const requestedNo = req.params.id;
+    // const sBlog =noteModel
+    noteModel.findById(requestedNo, function (err, resultItem) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.setHeader("Content-Type", "text/html")
+            res.render("adminSinglePage", ({ singleBlogMain: resultItem }))
+        }
+
+    });
+}
+
+const deleteSingleBlog = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const note = await noteModel.findByIdAndRemove(id);
+        // res.status(202).json(note);
+        res.redirect("/admin/posts")
+    } catch (error) {
+        await noteModel.findByIdAndUpdate(id, newNote, { new: true });
+        res.redirect("/admin/posts")
+        
+    }
+
+}
+
+module.exports = { adminLoginPage, adminRegisterPage, adminLogin, adminRegister, getAllUserBlog, getSingleBlog,deleteSingleBlog };
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const adminRouter = express.Router();
 // const auth = require("../middlewares/auth")
-const {adminLoginPage,adminRegisterPage,adminLogin,adminRegister} = require("../controllers/adminController")
+const {adminLoginPage,adminRegisterPage,adminLogin,adminRegister,getAllUserBlog,getSingleBlog,deleteSingleBlog} = require("../controllers/adminController")
 
 
 adminRouter.get("/login",adminLoginPage)
 adminRouter.get("/register",adminRegisterPage)
 adminRouter.post("/login",adminLogin)
 adminRouter.post("/register",adminRegister)
+adminRouter.get("/posts",getAllUserBlog)
+adminRouter.get("/posts/:id",getSingleBlog)
+adminRouter.get("/posts/delete/:id",deleteSingleBlog)
+
 
 module.exports = adminRouter;
